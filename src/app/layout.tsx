0692419dfe11886ce15d9b31bfb64e57adf4e02b
Your New Name <vipinkumarti555@gmail.com>
2024-07-18 22:21:44 +0530

    added authentication and middleware

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Recursive } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+
+const recursive= Recursive({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={recursive.className}>
+      
+        {children}</body>
     </html>
   );
 }
