2816eaec6a569077cbd52fe75f1ad72a6025880a
Your New Name <vipinkumarti555@gmail.com>
2025-04-26 21:59:43 +0530

    bruhhhhh

--- src/app/accounts/layout.tsx
+++ src/app/accounts/layout.tsx
@@ -4,9 +4,9 @@ import React from "react";
 const layout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="">
-      <div className="flex w-full relative h-full flex-1 lg:gap-6 ">
+      <div className="flex w-full relative h-full flex-1 lg:gap-6 py-2">
         <AmountSideBar />
-        <div className="flex h-fit flex-1 flex-col  w-full">
+        <div className="flex h-fit flex-1 flex-col  w-full ">
           {children}
         </div>
       </div>
